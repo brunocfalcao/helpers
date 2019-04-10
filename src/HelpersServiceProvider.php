f577d8f06d5ffde6cf7b9af5836e6172b3b0fcfb
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-04-10 20:30:26 +0200

    Update HelpersServiceProvider.php

--- src/HelpersServiceProvider.php
+++ src/HelpersServiceProvider.php
@@ -4,17 +4,12 @@ namespace Brunocfalcao\Helpers;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class HelpersServiceProvider extends BaseServiceProvider
+class HelpersServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // MySQL? Load the default string length to 191 chars.
-        if (config('database.connections.'.config('database.default').'.driver') == 'mysql') {
-            Schema::defaultStringLength(191);
-        }
-
         $this->registerMacros();
     }
 
