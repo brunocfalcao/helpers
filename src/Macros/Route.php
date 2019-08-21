86ced5594a058121e13894c528265992a99057ea
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-08-22 00:49:49 +0200

    Create Route.php

---
+++ src/Macros/Route.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brunocfalcao\Me\Macros;
+
+use Illuminate\Support\Facades\Route;
+
+Route::macro('name', function () : string {
+    return $this->getCurrentRoute()->getName();
+});
